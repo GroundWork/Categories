ef6ed26bb4fa983ff00c1858926d56fef0ce6026
Wess Cope <wcope@me.com>
2012-09-21 11:03:01 -0400

    Initial Commit

---
+++ Categories/Categories.h
@@ -0,0 +1,13 @@
+//
+//  Categories.h
+//  Categories
+//
+//  Created by Wess Cope on 9/21/12.
+//  Copyright (c) 2012 GroundWork. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Categories : NSObject
+
+@end
---
+++ Categories/Categories.m
@@ -0,0 +1,13 @@
+//
+//  Categories.m
+//  Categories
+//
+//  Created by Wess Cope on 9/21/12.
+//  Copyright (c) 2012 GroundWork. All rights reserved.
+//
+
+#import "Categories.h"
+
+@implementation Categories
+
+@end
